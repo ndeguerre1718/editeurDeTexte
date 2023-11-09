d3d0f4e3768a084d75f0faa253ba076a62181d19
Deguerre Nathan <nathan.deguerre@etu.umontpellier.fr>
2023-11-09 15:23:54 +0100

    feat ajoutDesciptionCommande

--- src/main/java/fr/iut/editeur/commande/CommandeClear.java
+++ src/main/java/fr/iut/editeur/commande/CommandeClear.java
@@ -9,13 +9,21 @@ public class CommandeClear extends CommandeDocument{
 
     @Override
     public void executer() {
-        if(parameters.length < 1) {
+        if(parameters[0].equals("clear") &&(parameters.length==1)){
+            System.out.println(getDescriptionCommande());
+        } else if(parameters.length < 1) {
             if (!parameters[3].isEmpty()) {
                 System.err.println("Format attendu : remplacer;debut;fin;texte");
                 return;
             }
         }
         this.document.clear();
+        this.document.ajouter(getDescriptionCommande());
         super.executer();
+    }
+
+    @Override
+    public String getDescriptionCommande() {
+        return "Commande : clear";
     }
 }
--- src/main/java/fr/iut/editeur/commande/CommandeMajuscule.java
+++ src/main/java/fr/iut/editeur/commande/CommandeMajuscule.java
@@ -2,6 +2,8 @@ package fr.iut.editeur.commande;
 
 import fr.iut.editeur.document.Document;
 
+import java.util.Objects;
+
 public class CommandeMajuscule extends CommandeDocument{
 
     public CommandeMajuscule(Document document, String[] parameters) {
@@ -10,13 +12,21 @@ public class CommandeMajuscule extends CommandeDocument{
 
     @Override
     public void executer() {
-        if(parameters.length < 3) {
+        if(parameters[0].equals("majuscules") &&(parameters.length==1)){
+            System.out.println(getDescriptionCommande());
+        }else if(parameters.length < 3) {
             System.err.println("Format attendu : masjucules;depart;fin");
-            return;
+        }else {
+
+            int debut = Integer.parseInt(parameters[1]);
+            int fin = Integer.parseInt(parameters[2]);
+            this.document.majuscules(debut, fin);
+            super.executer();
         }
-        int debut = Integer.parseInt(parameters[1]);
-        int fin = Integer.parseInt(parameters[2]);
-        this.document.majuscules(debut,fin);
-        super.executer();
+    }
+
+    @Override
+    public String getDescriptionCommande() {
+        return "/ Commande : majuscules";
     }
 }
